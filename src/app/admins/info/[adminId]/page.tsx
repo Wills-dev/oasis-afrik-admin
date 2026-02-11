21318ed8aff98a66bf06ab62564facd275e4e11c
victor <ejioguvictor6@gmail.com>
2026-02-11 15:40:41 +0100

    added: create admin modal and hooks

---
+++ src/app/admins/info/[adminId]/page.tsx
@@ -0,0 +1,7 @@
+import DashboardLayout from "@/components/templates/DashboardLayout/DashboardLayout";
+
+const UserInfoPage = () => {
+  return <DashboardLayout title="Admin info">UserInfoPage</DashboardLayout>;
+};
+
+export default UserInfoPage;
